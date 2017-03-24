1dd5720d74a9534abf3d02235894f612a15bfdd2
FKLam <lfk0120@163.com>
2017-03-24 09:33:19 +0800

    初始化项目

---
+++ RAC+MVVM_01/RAC+MVVM_01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAC+MVVM_01
+//
+//  Created by amglfk on 17/3/24.
+//  Copyright © 2017年 amglfk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RAC+MVVM_01/RAC+MVVM_01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RAC+MVVM_01
+//
+//  Created by amglfk on 17/3/24.
+//  Copyright © 2017年 amglfk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
